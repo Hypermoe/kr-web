829ac2cfbdab6633af5fa56f46860e2591137415
mikunailan <mikunailan@163.com>
2024-01-29 17:41:33 +0800

    Update file and fix some error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  app: {
+    head:{
+      title: "Kawaiirori",
+      script: [
+        {
+          src: "https://res.hypermoe.com/data/js/jquery/jquery-3.5.1.min.js",
+          type: "text/javascript"
+        },
+        {
+          src: "https://res.hypermoe.com/data/bootstrap/5.0.2/js/bootstrap.min.js",
+          type: "text/javascript"
+        },
+        {
+          src: "https://cdnjs.loli.net/ajax/libs/smoothscroll/1.4.9/SmoothScroll.min.js",
+          type: "text/javascript"
+        }
+      ]
+    }
+  },
+
+  css:[
+    "./assets/base.css",
+    "./assets/bootstrap-5.0.2-dist/css/bootstrap.min.css",
+    "./assets/font-awesome-4.7.0/css/font-awesome.min.css"
+  ]
+  
 })
--- pages/index.vue
+++ pages/index.vue
@@ -4,3 +4,98 @@ import krNav from '../components/krNav.vue'
 <template>
     <krNav/>
 </template>
+
+<style scoped>
+@font-face {
+    font-family: 'CaviarDreams';
+    src: url('https://res.hypermoe.com/data/font/CaviarDreams_Bold.ttf');
+}
+
+.pink {
+  text-decoration: none;
+  color: hsla(345, 100%, 75%, 1);
+  transition: 0.4s;
+}
+.full-page{
+    display: block;
+    width: 100%;
+    min-height: 100vh;
+}
+.pic-cover{
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position-x: center;
+}
+.full-mask{
+    background-image: linear-gradient(rgb(50, 50, 50), rgba(50, 50, 50, 0.15));
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+}
+.valign {
+    display: -webkit-box!important;
+    display: -ms-flexbox!important;
+    display: flex!important;
+    -webkit-box-align: center!important;
+    -ms-flex-align: center!important;
+    align-items: center!important;
+}
+.cover-header {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+
+    font-weight: normal;
+}
+.logo {
+    font-family: CaviarDreams;
+    display: block;
+    margin: 0 2rem 0 0;
+    font-size: 50px;
+    color: white;
+}
+.chntitle {
+    font-weight: 500;
+    font-size: 2.6rem;
+    position: relative;
+    top: -10px;
+}
+
+.engtitle {
+    font-size: 1.2rem;
+    color: white;
+}
+
+.greetings .chntitle,
+.greetings .engtitle {
+    text-align: left;
+}
+
+@media screen and (max-width:1024px) {
+    .rc {
+        line-height: 1.5;
+    }
+    .logo {
+        margin: 0 auto 2rem;
+        font-size: 23px;
+    }
+}
+@media all and (min-width: 1024px) {
+    .cover-header {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        padding: 0 2rem;
+    }
+    .rc {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
+    .rc .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
+}
+</style>
